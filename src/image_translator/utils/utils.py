2fb5b7f4c26de877fc54d30f033439dc8bdf866a
Giulia Caglia <giulia.caglia@objectiveplatform.com>
2024-05-15 10:48:37 +0200

    Bugfix (utils): Assign name to logger

--- src/image_translator/utils/utils.py
+++ src/image_translator/utils/utils.py
@@ -6,4 +6,4 @@ import logging
 def get_logger(name: str) -> logging.Logger:
     logging.basicConfig(format="%(asctime)s %(name)s - %(message)s", level=logging.INFO)
 
-    return logging.getLogger()
+    return logging.getLogger(name=name)
