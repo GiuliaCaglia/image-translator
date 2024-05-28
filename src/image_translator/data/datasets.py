add4c28c11682a1586b6a7cb6fe5e6dd54c0d384
Giulia Caglia <cagliagiulia42@gmail.com>
2024-05-28 19:19:07 +0200

    Config (datasets): Don't normalize images

--- src/image_translator/data/datasets.py
+++ src/image_translator/data/datasets.py
@@ -33,7 +33,6 @@ class ImageDataset(Dataset):
         [
             transforms.Resize((256, 256)),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
     )
 
